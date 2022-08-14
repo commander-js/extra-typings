35c3c39730782ee09c523eb34c9ef4df0d646167
John Gee <john@ruru.gen.nz>
2022-08-14 17:33:07 +1200

    Remove default export of global program

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const commander = require('commander');
-// const { Argument } = require('./lib/argument.js');
-// const { Command } = require('./lib/command.js');
-// const { CommanderError, InvalidArgumentError } = require('./lib/error.js');
-// const { Help } = require('./lib/help.js');
-// const { Option } = require('./lib/option.js');
-
 
 // @ts-check
 
-// Return a different global program than commander, but otherwise same pattern as upstream.
+exports = module.exports = {};
 
-exports = module.exports = new commander.Command();
-exports.program = exports; // More explicit access to global command.
-// Implicit export of createArgument, createCommand, and createOption.
+// Return a different global program than commander,
+// and don't also return it as default export.
+exports.program = new commander.Command();
 
 /**
  * Expose classes. The FooT versions are just types, so return Commander original implementations!
@@ -25,3 +19,7 @@ exports.Help = commander.Help;
 exports.InvalidArgumentError = commander.InvalidArgumentError;
 exports.InvalidOptionArgumentError = commander.InvalidArgumentError; // Deprecated
 exports.Option = commander.Option;
+
+exports.createArgument = () => new commander.Argument();
+exports.createCommand = () => new commander.Command();
+exports.createOption = () => new commander.Option();
