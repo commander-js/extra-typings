9a650e99ccdac0bb3b3d2dc7ad0784ed4f213967
John Gee <john@ruru.gen.nz>
2022-09-04 18:48:30 +1200

    Add test

--- tests/help.test-d.ts
+++ tests/help.test-d.ts
@@ -1,9 +1,18 @@
 import { expectType } from 'tsd';
-import { Help, CommandWeakOpts } from '..';
+import { Help, CommandWeakOpts, Option } from '..';
 
 if ('when subclass Help then cmd is weakly typed') {
   class MyHelp extends Help {
     subcommandTerm(cmd: CommandWeakOpts) {
+      return '';''
+    }
+  }
+}
+
+if ('when subclass Help then option is weakly typed') {
+  class MyHelp extends Help {
+    optionTerm(option: Option) {
+      const n = option.name();
       return '';''
     }
   }
