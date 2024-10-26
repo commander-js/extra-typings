5a90c8f54476663052b4b6d35151aa452b4bf096
John Gee <john@ruru.gen.nz>
2024-10-27 12:52:24 +1300

    Remove dummy if() blocks mirroring Jest it() tests (#77)

--- tests/assignment.test-d.ts
+++ tests/assignment.test-d.ts
@@ -1,12 +1,14 @@
 import { expectAssignable, expectNotAssignable } from 'tsd';
 import { Command, CommandUnknownOpts, Option } from '..';
 
-if ('when assign Command to CommandUnknownOpts then no error') {
+// 'when assign Command to CommandUnknownOpts then no error'
+{
   const c = new Command();
   expectAssignable<CommandUnknownOpts>(c);
 }
 
-if ('when assign CommandUnknownOpts to Command then error') {
+// 'when assign CommandUnknownOpts to Command then error'
+{
   const custom: CommandUnknownOpts = new Command();
   expectNotAssignable<Command>(custom);
 }
