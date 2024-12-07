b4af62568dfda86934da280c59621d01625957d8
John Gee <john@ruru.gen.nz>
2024-12-08 10:47:43 +1300

    Update types and tests with new methods

--- tests/commander.test-d.ts
+++ tests/commander.test-d.ts
@@ -12,7 +12,7 @@ import { expectType, expectAssignable } from 'tsd';
 
 function expectChainedCommand<T extends commander.CommandUnknownOpts>(cmd: T) {}
 
-// We are are not just checking return types here, we are also implicitly checking that the expected syntax is allowed.
+// We are not just checking return types here, we are also implicitly checking that the expected syntax is allowed.
 
 /* eslint-disable @typescript-eslint/no-empty-function */
 
@@ -539,11 +539,16 @@ expectChainedCommand(
   program.configureOutput({
     writeOut: (str: string) => console.log(str),
     writeErr: (str: string) => console.error(str),
-    getOutHelpWidth: () => 80,
-    getErrHelpWidth: () => 80,
     outputError: (str: string, write: (str: string) => void) => {
       write(str);
     },
+
+    getOutHelpWidth: () => 80,
+    getErrHelpWidth: () => 80,
+
+    getOutHasColors: () => true,
+    getErrHasColors: () => true,
+    stripColor: (str) => str,
   }),
 );
 
@@ -553,7 +558,11 @@ const helperCommand = new commander.Command();
 const helperOption = new commander.Option('-a, --all');
 const helperArgument = new commander.Argument('<file>');
 
+helper.prepareContext({});
+helper.prepareContext({ helpWidth: 120, error: true, outputHasColors: false });
+
 expectType<number | undefined>(helper.helpWidth);
+expectType<number>(helper.minWidthToWrap);
 expectType<boolean>(helper.sortSubcommands);
 expectType<boolean>(helper.sortOptions);
 expectType<boolean>(helper.showGlobalOptions);
@@ -580,8 +589,31 @@ expectType<number>(helper.longestGlobalOptionTermLength(helperCommand, helper));
 expectType<number>(helper.longestArgumentTermLength(helperCommand, helper));
 expectType<number>(helper.padWidth(helperCommand, helper));
 
-expectType<string>(helper.wrap('a b c', 50, 3));
+expectType<number>(helper.displayWidth('some string'));
+expectType<string>(helper.boxWrap('a b c', 50));
+expectType<string>(
+  helper.formatItem('--example', 12, 'example description', helper),
+);
+expectType<boolean>(helper.preformatted('a\nb c'));
 
+expectType<string>(helper.styleTitle('Usage:'));
+
+expectType<string>(helper.styleUsage('foo [options] <file>'));
+expectType<string>(helper.styleCommandText('foo'));
+
+expectType<string>(helper.styleCommandDescription('description'));
+expectType<string>(helper.styleOptionDescription('description'));
+expectType<string>(helper.styleSubcommandDescription('description'));
+expectType<string>(helper.styleArgumentDescription('description'));
+expectType<string>(helper.styleDescriptionText('description'));
+
+expectType<string>(helper.styleOptionTerm('-a, --all'));
+expectType<string>(helper.styleSubcommandTerm('bar [options]'));
+expectType<string>(helper.styleArgumentTerm('<file>'));
+
+expectType<string>(helper.styleOptionText('-a, --all'));
+expectType<string>(helper.styleSubcommandText('bar'));
+expectType<string>(helper.styleArgumentText('<file>'));
 expectType<string>(helper.formatHelp(helperCommand, helper));
 
 // Option properties
